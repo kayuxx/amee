e91859e9afcbf7f835249ab4a41bdd99c5fcd7e7
Zoheir Benghalem <kayuxx@tutanota.com>
2024-08-22 19:45:16 +0100

    remove sourcemap option from tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig([
-  {
-    entry: {
-      index: "src/index.ts",
-      "providers/index": "./src/oauth/index.ts",
-    },
-    format: ["esm", "cjs"],
-    dts: true,
-    clean: true,
-    treeshake: true,
-    sourcemap: true,
+export default defineConfig({
+  entry: {
+    index: "src/index.ts",
+    "providers/index": "./src/oauth/index.ts"
   },
-]);
+  format: ["esm", "cjs"],
+  dts: true,
+  clean: true,
+  treeshake: true
+});
