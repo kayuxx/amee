0a52ae42449e9a787f6612b82db046e2abf22f7e
Zoheir Benghalem <kayuxx@tutanota.com>
2024-09-28 22:22:16 +0100

    enable react strict mode

--- nextjs/next.config.mjs
+++ nextjs/next.config.mjs
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "lh3.googleusercontent.com"
+        hostname: "lh3.googleusercontent.com",
       },
       {
         protocol: "https",
-        hostname: "avatars.githubusercontent.com"
-      }
-    ]
-  }
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
